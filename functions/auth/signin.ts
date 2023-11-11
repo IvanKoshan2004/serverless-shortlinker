a13fb538ed527d89ad395c67579f6af52300ded5
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-11 21:58:52 +0200

    implemented signin and signup functions

--- functions/auth/signin.ts
+++ functions/auth/signin.ts
@@ -1,1 +1,66 @@
-export async function handler() {}
+import { APIGatewayEvent } from "aws-lambda";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { SignInDto } from "../../types/dtos/signin.dto";
+import { compare } from "bcryptjs";
+import { User } from "../../types/dtos/model/user.type";
+
+export async function handler(event: APIGatewayEvent) {
+    if (event.body === null) {
+        return {
+            statusCode: 400,
+            body: "Request should contain a body",
+        };
+    }
+    let signInDto: SignInDto;
+    try {
+        signInDto = JSON.parse(event.body);
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: "Invalid body",
+        };
+    }
+    if (!signInDto.email) {
+        return {
+            statusCode: 400,
+            body: "Request body should have email",
+        };
+    } else if (!signInDto.password) {
+        return {
+            statusCode: 400,
+            body: "Request body should have password",
+        };
+    }
+    console.log(process.env.DYNAMODB_EMAIL_INDEX);
+    const dynamodb = new DynamoDBClient({ endpoint: "http://localhost:8000" });
+    const command = new QueryCommand({
+        TableName: process.env.DYNAMODB_USER_TABLE,
+        IndexName: process.env.DYNAMODB_EMAIL_INDEX,
+        KeyConditionExpression: "email = :email",
+        ExpressionAttributeValues: {
+            ":email": { S: signInDto.email },
+        },
+    });
+    const result = await dynamodb.send(command);
+    if (!result.Items || result.Items.length == 0) {
+        return {
+            statusCode: 404,
+            body: "User email or password is invalid",
+        };
+    }
+    const user = result.Items[0] as User;
+    const isAuthenticated = await compare(
+        signInDto.password,
+        user.passwordHash.S
+    );
+    if (!isAuthenticated) {
+        return {
+            statusCode: 404,
+            body: "User email or password is invalid",
+        };
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ accessToken: "jwt_goes_here" }),
+    };
+}
--- functions/auth/signup.ts
+++ functions/auth/signup.ts
@@ -1,1 +1,78 @@
-export async function handler() {}
+import { APIGatewayEvent } from "aws-lambda";
+import {
+    DynamoDBClient,
+    PutItemCommand,
+    QueryCommand,
+} from "@aws-sdk/client-dynamodb";
+import { SignUpDto } from "../../types/dtos/signup.dto";
+import { genSalt, hash } from "bcryptjs";
+import { randomUUID } from "crypto";
+
+export async function handler(event: APIGatewayEvent) {
+    if (event.body === null) {
+        return {
+            statusCode: 400,
+            body: "Request should contain a body",
+        };
+    }
+    let signUpDto: SignUpDto;
+    try {
+        signUpDto = JSON.parse(event.body);
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: "Invalid body",
+        };
+    }
+    if (!signUpDto.email) {
+        return {
+            statusCode: 400,
+            body: "Request body should have email",
+        };
+    } else if (!signUpDto.password) {
+        return {
+            statusCode: 400,
+            body: "Request body should have password",
+        };
+    }
+    const dynamodb = new DynamoDBClient({ endpoint: "http://localhost:8000" });
+    const emailExistsCheck = await dynamodb.send(
+        new QueryCommand({
+            TableName: process.env.DYNAMODB_USER_TABLE,
+            IndexName: process.env.DYNAMODB_EMAIL_INDEX,
+            KeyConditionExpression: "email = :email",
+            ExpressionAttributeValues: {
+                ":email": { S: signUpDto.email },
+            },
+        })
+    );
+    if (emailExistsCheck.Items && emailExistsCheck.Items.length != 0) {
+        return {
+            statusCode: 409,
+            body: JSON.stringify("User with this email already exists"),
+        };
+    }
+    const userId = randomUUID();
+    const salt = await genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS!));
+    const generatedHash = await hash(signUpDto.password, salt);
+    await dynamodb.send(
+        new PutItemCommand({
+            TableName: process.env.DYNAMODB_USER_TABLE!,
+            Item: {
+                userId: {
+                    S: userId,
+                },
+                email: {
+                    S: signUpDto.email,
+                },
+                passwordHash: {
+                    S: generatedHash,
+                },
+            },
+        })
+    );
+    return {
+        statusCode: 201,
+        body: JSON.stringify({ accessToken: "jwt_goes_here" }),
+    };
+}

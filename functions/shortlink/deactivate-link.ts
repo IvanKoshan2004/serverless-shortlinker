9280e0ed82ddc388aef7bc2192c6fa19a1bc63ee
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-16 14:03:47 +0200

    fix deactivate-link function

--- functions/shortlink/deactivate-link.ts
+++ functions/shortlink/deactivate-link.ts
@@ -3,12 +3,15 @@ import { extractBearerToken } from "../lib/extract-bearer-token";
 import { authorizeJwtToken } from "../lib/authorize-jwt-token";
 import {
     DynamoDBClient,
+    GetItemCommand,
+    GetItemCommandOutput,
     UpdateItemCommand,
     UpdateItemCommandOutput,
 } from "@aws-sdk/client-dynamodb";
 import { UserJwtPayload } from "../../types/model/user-jwt.type";
 import { createJsonResponse } from "../lib/create-json-response";
 import { DeactivateLinkRO } from "../../types/ros/deactivate-link.ro";
+import { ShortLink } from "../../types/model/short-link.type";
 
 export async function handler(
     event: APIGatewayEvent
@@ -18,21 +21,53 @@ export async function handler(
     );
     const verifyJwtRO = await authorizeJwtToken<UserJwtPayload>(accessToken);
     if (!verifyJwtRO.success) {
-        return createJsonResponse(400, {
+        return createJsonResponse(401, {
             success: false,
             error: verifyJwtRO.error,
         });
     }
     const linkId = event.pathParameters?.linkId;
     if (!linkId) {
-        return createJsonResponse(404, {
+        return createJsonResponse(400, {
             success: false,
-            error: "Link with this id not found",
+            error: "Link id parameter should be specified in the path",
         });
     }
     const dynamodb = new DynamoDBClient({
         endpoint: process.env.IS_OFFLINE ? "http://localhost:8000" : undefined,
     });
+    let linkGet: GetItemCommandOutput;
+    try {
+        linkGet = await dynamodb.send(
+            new GetItemCommand({
+                TableName: process.env.DYNAMODB_SHORTLINK_TABLE,
+                Key: {
+                    linkId: {
+                        S: linkId,
+                    },
+                },
+            })
+        );
+    } catch (e) {
+        console.log(e);
+        return createJsonResponse(500, {
+            success: false,
+            error: "Failed to deactivate a link",
+        });
+    }
+    const link = linkGet.Item as ShortLink;
+    if (!link) {
+        return createJsonResponse(404, {
+            success: false,
+            error: "Short link not found",
+        });
+    }
+    if (link.userId.S !== verifyJwtRO.data.payload.userId) {
+        return createJsonResponse(401, {
+            success: false,
+            error: "User unauthorized",
+        });
+    }
     let linkDeactivateUpdate: UpdateItemCommandOutput;
     try {
         linkDeactivateUpdate = await dynamodb.send(
